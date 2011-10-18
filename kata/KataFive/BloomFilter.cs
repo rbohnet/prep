a5569317feb025f67c4290858b476b6dfedda770
Ralph Bohnet <rbohnet@gmail.com>
2011-10-18 09:46:56 -0600

    Pushing new changes

---
+++ kata/KataFive/BloomFilter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KataFive {
+    
+    public class BloomFilter {
+
+
+        
+    }
+}
+
+
+/*
+Test: 
+ * setup:
+ * load dictionary
+ * 
+ * 1) Search for existing word
+ * verify found
+ * 
+ * 2) Search for non-existent word
+ * 
+ * 
+ * 
+
+*/
