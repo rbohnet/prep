1828500808e0b910d636c80bbe19954442f164b8
Ralph Bohnet <rbohnet@gmail.com>
2011-10-21 09:36:21 -0600

    Pushing new changes

---
+++ kata/KataFive.specs/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KataFive.specs {
+
+
+    public class when_asdfsdfaasdfasdf
+    {
+        private It should_sdfsdfsdf = () =>
+                              
+    }
+}
